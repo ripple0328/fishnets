fa44a9af97e4a5d52810991c23ca97f23fed5fdc
ripple <ripple0328@gmail.com>
2013-03-15 22:51:18 +0800

    add config.ru gemfile and private

---
+++ config/environment.rb
@@ -0,0 +1,4 @@
+require 'rubygems'
+require 'sinatra'
+require File.join(File.dirname(__FILE__),'private')
+require File.join(File.dirname(__FILE__),'..','main')
--- main.rb
+++ main.rb
@@ -1,11 +1,15 @@
-require 'sinatra'
-
 get '/'  do
   "hello world"  
 end
 
 
-get 'access' do
-  
+get '/access' do
+  @pre_digest = [token, request[:timestamp],request[:nonce]].sort.join
+  @digest = Digest::SHA1.hexdigest(@pre_digest)
+  @echo = request[:echostr]
+  if @digest == request[:signature]
+    @echo
+  else
+    'Error ' + @echo
+  end
 end
-
