034683841740aec71b4e2d6e146114f20b9369bc
ripple <ripple0328@gmail.com>
2013-03-16 13:38:40 +0800

    move main to main.bak

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
 require 'weixin/model'
+require 'weixin/middleware'
 require File.join(File.dirname(__FILE__),'private')
 require File.join(File.dirname(__FILE__),'..','main')
