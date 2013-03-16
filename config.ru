3ee2b69ff6bed26659d0fdbe4b2dfde7f7c5d92a
ripple <ripple0328@gmail.com>
2013-03-17 00:57:21 +0800

    recover private file

--- config.ru
+++ config.ru
@@ -1,2 +1,3 @@
 require File.join(File.dirname(__FILE__),'config','environment')
 run Sinatra::Application
+
---
+++ config/private.rb
@@ -0,0 +1,3 @@
+TOKEN = 'fishnetsapp'
+XIAOI = 'http://nlp.xiaoi.com/robot/demo/wap/wap-demo.action'
+CHATBOT_API='http://doido.sinaapp.com/chat/'
