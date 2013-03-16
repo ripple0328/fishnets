07e78ec62fdc7b5dac01bb2e97a83cb6916caeec
ripple <ripple0328@gmail.com>
2013-03-16 08:55:09 +0800

    add access

--- config/private.rb
+++ config/private.rb
@@ -1,1 +1,3 @@
 TOKEN = 'fishnetsapp'
+XIAOI = 'http://nlp.xiaoi.com/robot/demo/wap/wap-demo.action'
+DOIDO='http://doido.sinaapp.com/chat/'
--- main.rb
+++ main.rb
@@ -12,3 +12,31 @@ get '/access' do
     'Error ' + @echo
   end
 end
+
+
+post '/access'  do
+  @body = request.body
+  @page = Nokogiri::XML(@body)
+  @msg = @page.at_xpath('xml/Content').text
+  @dev = @page.at_xpath('xml/ToUserName').text
+  @user = @page.at_xpath('xml/FromUserName').text
+  @msg_type = @page.at_xpath('xml/MsgType').text
+  @create_time =@page.at_xpath('xml/CreateTime').text
+  @url = DOIDO
+  @post_param = {'say' => @msg}
+  @return = Net::HTTP.post_form(URI.parse(@url),@post_param)
+  @replay = Nokogiri::HTML(@return.body).text
+
+  @to_user = "
+   <xml>
+  <ToUserName><![CDATA[#{@user}]]></ToUserName>
+  <FromUserName><![CDATA[#{@dev}]]></FromUserName>
+  <CreateTime>#{Time.now}</CreateTime>
+  <MsgType><![CDATA[#{@msg_type}]]></MsgType>
+  <Content><![CDATA[#{@replay}]]></Content>
+  <FuncFlag>0</FuncFlag>
+  </xml> "
+
+  return @to_user
+
+end
