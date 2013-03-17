fb33590deba7a63871543c53a51a596fa6b58722
ripple <ripple0328@gmail.com>
2013-03-17 23:04:02 +0800

    remove private

--- config/private.rb
+++
@@ -1,4 +0,0 @@
-TOKEN = 'fishnetsapp'
-CHAT_XIAOI_API = 'http://nlp.xiaoi.com/robot/demo/wap/wap-demo.action'
-CHAT_DOIDO_API='http://doido.sinaapp.com/chat/'
-CHAT_XIAODOU_API='http://xiao.douqq.com/bot/chata.php?chat='
