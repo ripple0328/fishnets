86ac6a5e2120e8f2d7bccbdadad29f6dc80c15c0
ripple <ripple0328@gmail.com>
2013-03-22 16:27:21 +0800

    add readme intro

--- config/private.rb
+++ config/private.rb
@@ -2,3 +2,6 @@ TOKEN = 'fishnetsapp'
 CHAT_XIAOI_API = 'http://nlp.xiaoi.com/robot/demo/wap/wap-demo.action'
 CHAT_DOIDO_API='http://doido.sinaapp.com/chat/'
 CHAT_XIAODOU_API='http://xiao.douqq.com/bot/chata.php?chat='
+
+WORLFRAM_APPID="KQK7JP-HKXYAXQ279"
+# http://products.wolframalpha.com/docs/WolframAlpha-API-Reference.pdf
